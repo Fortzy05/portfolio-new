ce4948d35a178ceecf977ddfbc6622d90b9e2427
Fortzy05 <omorodion.fortune@gmail.com>
2025-01-03 20:02:07 +0100

    added react-router-dom to package.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-
-
-import './App.css'
-
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
- 
-
   return (
-   <main className=''>portfolio</main>
-  )
+    <main className="text-5xl uppercase">
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="codes" element={<Codes />} />
+        </Routes>
+      </BrowserRouter>
+    </main>
+  );
 }
 
-export default App
+export default App;
