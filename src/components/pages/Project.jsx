53eeb847a82f73b575625b710f9353351d977da5
Fortzy05 <omorodion.fortune@gmail.com>
2025-01-21 12:02:13 +0100

    css styling adjustments

--- src/components/pages/Project.jsx
+++ src/components/pages/Project.jsx
@@ -9,12 +9,23 @@ function Project() {
       <div className={styles.pdesc}>
         <div>Here are some of the personal projects that I have done</div>
       </div>
-      <div className={styles.projects}>
+      <div className="grid md:grid-cols-3 grid-cols-1 auto-cols-auto  ">
         {PersonalProjects.map((proj, idx) => (
-          <div key={idx} className={`styles.pbox`}>
-            <img src={proj.images} alt={proj.name} />
-            <div className={styles.pname}>{proj.name}</div>
-            <div className={styles.desc}>{proj.description}</div>
+          <div
+            key={idx}
+            className={`{styles.pbox}  w-[350px] text-center md:w-[460px] shadow-sm  shadow-[#4a5466] mb-[2rem]  `}
+          >
+            <img
+              className="object-contain w-[350px]  md:w-[460px] text-center"
+              src={proj.images}
+              alt={proj.name}
+            />
+            <div
+              className="text-[#4a5466] text-center text-[1.5rem] font-bold leading-[3]"
+            >
+              {proj.name}
+            </div>
+            <div className="text-[#4a5466] text-center leading-[3]">{proj.description}</div>
             <div className={styles.link}>
               <a href={proj.link}>Link</a>
             </div>
